f4fb62c7b7c0f19e2a7de5ee1c5f57facb503a3e
christophberger <christophberger@users.noreply.github.com>
2023-10-12 18:44:59 +0200

    feat: announce the server URL in the log output so that users can click the URL in the terminal

--- router.go
+++ router.go
@@ -19,6 +19,7 @@ func startServer(a *app) {
 
 	// Start the server
 	go func() {
+		log.Println("Listening on http://localhost:8020")
 		log.Fatal(http.ListenAndServe("localhost:8020", mux))
 	}()
 }
